00afae7c4e425fd4dce47056fb736b37873c2063
Todd Demone <todd.demone.dev@gmail.com>
2022-02-03 12:03:31 +0000

    move three routes from routes/stories.js to this file

--- routes/contributions.js
+++ routes/contributions.js
@@ -86,5 +86,77 @@ module.exports = (db) => {
       });
   });
 
+  // GET TOP-LEVEL CONTRIBUTIONS (FOR HOME PAGE)
+  router.get("/alltoplevel", (req, res) => {
+    const limit = 10;
+    const queryString = `
+      SELECT contributions.*, stories.title as story_title
+      FROM contributions
+      JOIN stories ON stories.id = story_id
+      WHERE working_level = 0
+      ORDER BY id DESC
+      LIMIT $1;
+    `;
+    const queryParams = [limit];
+    db.query(queryString, queryParams)
+      .then(data => {
+        const topLevelContribs = data.rows;
+        res.json({ topLevelContribs });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  // GET TOP-LEVEL PLUS ACCEPTED CONTRIBS FOR A STORY
+  router.get("/story/:story_id/accepted", (req, res) => {
+    const { story_id } = req.params;
+    const queryString = `
+      SELECT contributions.*, stories.title as story_title
+      FROM contributions
+      JOIN stories ON stories.id = story_id
+      WHERE story_id = $1
+      AND accepted = true
+      ORDER BY working_level ASC;
+    `;
+    const queryParams = [story_id];
+    db.query(queryString, queryParams)
+      .then(data => {
+        const acceptedContribs = data.rows;
+        res.json({ acceptedContribs });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  // GET UNACCEPTED CONTRIBS FOR A STORY
+  router.get("/story/:story_id/unaccepted", (req, res) => {
+    const { story_id } = req.params;
+    // TODO: amend query string to order contributions in 'thread' order
+    const queryString = `
+      SELECT contributions.*, stories.title as story_title
+      FROM contributions
+      JOIN stories ON stories.id = story_id
+      WHERE story_id = $1
+      AND accepted = false;
+    `;
+    const queryParams = [story_id];
+    db.query(queryString, queryParams)
+      .then(data => {
+        const unacceptedContribs = data.rows;
+        res.json({ unacceptedContribs });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
