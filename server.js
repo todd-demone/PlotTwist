ffd907b74e49124acb635e280f584e80d9296a7b
Todd Demone <todd.demone.dev@gmail.com>
2022-02-03 15:38:01 +0000

    remove ejs, remove index route, add cookie-session, add express.json()

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
+const cookieSession = require('cookie-session');
 const morgan = require("morgan");
 
 // PG database client/connection setup
@@ -19,7 +20,6 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
@@ -32,6 +32,11 @@ app.use(
 );
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -48,14 +53,6 @@ app.use("/api/contributions", contributionsRoutes(db));
 app.use("/api/votes", votesRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
