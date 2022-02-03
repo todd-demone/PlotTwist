343c3a6174004e328c5c40e0be0a33235793ab77
Todd Demone <todd.demone.dev@gmail.com>
2022-02-03 22:05:56 +0000

    refactor and name change to twists.js

--- routes/twists.js
+++ routes/twists.js
@@ -3,86 +3,29 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-
   //////////////////////////
   //////GET REQUESTS////////
   //////////////////////////
 
-  //GET USER'S CONTRIBUTIONS
-  router.get("user/:user_id", (req, res) => {
-    const { user_id } = req.params;
-
-    const queryString = `
-      SELECT contributions.*, count(votes) AS number_of_votes, stories.title AS story_title
-      FROM contributions
-      JOIN votes ON contributions.id = votes.contribution_id
-      JOIN stories ON stories.id = story_id
-      GROUP BY contributions.id
-      WHERE user_id = $1
-      ORDER BY date_created, story_id;
-    `;
-
-    const queryParams = [user_id];
-
-    db.query(queryString, queryParams)
-      .then(data => {
-        const contributions = data.rows;
-        res.json({ contributions });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-
-  // GET "TOP-LEVEL CONTRIBUTIONS" (FOR HOME PAGE)
-  router.get("/alltoplevel", (req, res) => {
-    const limit = 10;
-
-    const queryString = `
-      SELECT contributions.*, stories.title AS story_title
-      FROM contributions
-      JOIN stories ON stories.id = story_id
-      WHERE level = 0
-      ORDER BY date_created DESC
-      LIMIT $1;
-      `;
-
-    const queryParams = [limit];
-
-    db.query(queryString, queryParams)
-      .then(data => {
-        const topLevelContribs = data.rows;
-        res.json({ topLevelContribs });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  // GET TOP-LEVEL PLUS ACCEPTED CONTRIBS FOR A STORY
+  // GET ACCEPTED TWISTS FOR A STORY
   router.get("/story/:story_id/accepted", (req, res) => {
     const { story_id } = req.params;
     const queryString = `
-      SELECT contributions.*, count(votes) AS number_of_votes, stories.title as story_title
-      FROM contributions
-      JOIN votes ON contributions.id = votes.contribution_id
-      JOIN stories ON stories.id = story_id
-      GROUP BY contributions.id
-      WHERE story_id = $1 AND accepted = true
-      ORDER BY working_level ASC;
-      `;
+      SELECT twists.*, count(votes) AS number_of_votes
+      FROM twists
+      JOIN votes ON twists.id = votes.twist_id
+      GROUP BY twists.id
+      WHERE story_id = $1
+      AND accepted = true
+      ORDER BY level ASC;
+    `;
 
     const queryParams = [story_id];
 
     db.query(queryString, queryParams)
       .then(data => {
-        const acceptedContribs = data.rows;
-        res.json({ acceptedContribs });
+        const twists = data.rows;
+        res.json({ twists });
       })
       .catch(err => {
         res
@@ -91,22 +34,31 @@ module.exports = (db) => {
       });
   });
 
-  // GET ALL (INC. UNACCEPTED) CONTRIBS FOR A STORY
+  // GET UNACCEPTED TWISTS FOR A STORY
   router.get("/story/:story_id/unaccepted", (req, res) => {
     const { story_id } = req.params;
-    // TODO: amend query string to order contributions in 'thread' order
+    // TODO: amend query string to order twists in 'thread' order
     const queryString = `
-      SELECT contributions.*, count(votes) AS number_of_votes,stories.title as story_title
-      FROM contributions
-      JOIN votes ON contributions.id = votes.contribution_id
-      JOIN stories ON stories.id = story_id
-      WHERE story_id = $1;
+      SELECT twists.*, count(votes) AS number_of_votes
+      FROM twists
+      JOIN votes ON twists.id = votes.twist_id
+      WHERE story_id = $1
+      AND accepted = false
+      ORDER BY parent_id ASC NULLS FIRST;
       `;
+      /*
+      WITH RECURSIVE story_thread AS (
+      SELECT * FROM twists WHERE parent_id IS NULL
+      UNION ALL
+      SELECT t.* FROM twists AS t JOIN story_thread AS s ON t.parent_id = s.id
+      )
+      SELECT * FROM story_thread;
+      */
     const queryParams = [story_id];
     db.query(queryString, queryParams)
       .then(data => {
-        const unacceptedContribs = data.rows;
-        res.json({ unacceptedContribs });
+        const twists = data.rows;
+        res.json({ twists });
       })
       .catch(err => {
         res
@@ -115,21 +67,47 @@ module.exports = (db) => {
       });
   });
 
-  /////////////////////
-  ////POST REQUESTS////
-  /////////////////////
+  // GET AUTHOR'S TWISTS
+  router.get("author/:author_id", (req, res) => {
+    const { author_id } = req.params;
 
-  //POST A CONTRIBUTION
-  router.post("/", (req, res) => {
-    const { user_id } = req.session;
-    const { story_id, parent_id, working_level, text } = req.body
-    // WHERE $6 = stories.user_id contributions.accepted = true
     const queryString = `
-      INSERT INTO contributions (user_id, story_id, parent_id, working_level, text)
+      SELECT twists.*, count(votes) AS number_of_votes
+      FROM twists
+      JOIN votes ON twists.id = votes.twist_id
+      GROUP BY twists.id
+      WHERE author_id = $1
+      ORDER BY date_created, story_id;
+    `;
+
+    const queryParams = [author_id];
+
+    db.query(queryString, queryParams)
+      .then(data => {
+        const twists = data.rows;
+        res.json({ twists });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  
+  ///////////////////////////
+  ////POST & PUT REQUESTS////
+  ///////////////////////////
+
+  // POST A TWIST
+  router.post("/", (req, res) => {
+    const author_id = req.session.user_id;
+    const { story_id, parent_id, level, text } = req.body
+    const queryString = `
+      INSERT INTO twists (author_id, story_id, parent_id, level, text)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
     `;
-    const queryParams = [user_id, story_id, parent_id, working_level, text];
+    const queryParams = [author_id, story_id, parent_id, level, text];
 
     db.query(queryString, queryParams)
       .then(() => {
@@ -142,17 +120,20 @@ module.exports = (db) => {
       });
   });
 
-  //ACCEPT A CONTRIBUTION
-  router.put("/accept", (req, res) => {
-    const { user_id } = req.session;
-    const {contribution_id} = req.params;
+  // ACCEPT A TWIST
+  router.put("/:id/accept", (req, res) => {
+    const twist_id = req.params;
+    const author_id = req.session.user_id;
     const queryString = `
-      UPDATE contributions
-      SET accepted = true;
-      WHERE id = $1
+      UPDATE twists
+      SET accepted = true
+      FROM twists
+      JOIN stories ON stories.id = story_id
+      WHERE twists.id = $1
+      AND stories.author_id = $2
       RETURNING *;
     `;
-    const queryParams = [contribution_id];
+    const queryParams = [twist_id, author_id];
 
     db.query(queryString, queryParams)
       .then(() => {
@@ -165,18 +146,20 @@ module.exports = (db) => {
       });
   });
 
-  //DELETE CONTRIBUTION
+  // DELETE TWIST
   router.put("/:id/delete", (req, res) => {
     const { id } = req.params;
+    const author_id = req.session.user_id;
     queryString = `
-      UPDATE contributions SET text='[Deleted]'
+      UPDATE twists 
+      SET text='[Deleted]'
       WHERE id = $1
+      AND author_id = $2
       RETURNING *;
     `;
-    queryParams = [id];
+    queryParams = [id, author_id];
     db.query(queryString, queryParams)
       .then(data => {
-        const contribution = data.rows[0];
         res.status(200).send();
       })
       .catch(err => {
@@ -187,4 +170,5 @@ module.exports = (db) => {
   });
 
   return router;
+
 };
