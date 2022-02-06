02193e98121840457501564208d370d619dece35
Todd Demone <todd.demone.dev@gmail.com>
2022-02-06 17:11:23 +0000

    change NULL to 0 to make the getTwists query work; fix bug - change 4th seed's parent_id from 4 to 3 to start the nested twists

--- db/seeds/03_twists_seeds.sql
+++ db/seeds/03_twists_seeds.sql
@@ -1,10 +1,25 @@
-INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  1,  null, 'Mr. Dursley stuck his head out of the window.  Suddenly the owl grabbed him by his mop of hair and lifted him into the sky!');
-INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  2,  null, 'Harry Potter then came and magicked really good. The End.');
-INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  null, 'it was the age of wisdom, it was the age of foolishness,');
-INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  4, 'actually it was just the best of times.');
+INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  1,  0, 'Mr. Dursley stuck his head out of the window.  Suddenly the owl grabbed him by his mop of hair and lifted him into the sky!');
+INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  2,  0, 'Harry Potter then came and magicked really good. The End.');
+INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  0, 'it was the age of wisdom, it was the age of foolishness,');
+INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  3, 'actually it was just the best of times.');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  3,  4, 'it was the epoch of belief, it was the epoch of incredulity, ');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  1,  5, 'it was the season of Light, it was the season of Darkness, ');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  4,  6, 'it was the spring of hope, it was the winter of despair, ');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  1,  7, 'we had everything before us, we had nothing before us, ');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  8, 'we were all going direct to Heaven, we were all going direct the other way—in short, ');
 INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  3,  9, 'the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.');
+
+-- ----------------------------------------------------------------------------------------
+-- NOTE TO DRAFT: seeds have been rearranged to test the getTwists query in db/twistsDb.js
+-- ----------------------------------------------------------------------------------------
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  1,  0, 'Mr. Dursley stuck his head out of the window.  Suddenly the owl grabbed him by his mop of hair and lifted him into the sky!');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (1,  2,  0, 'Harry Potter then came and magicked really good. The End.');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  0, 'it was the age of wisdom, it was the age of foolishness,');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  3, 'actually it was just the best of times.');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  3,  0, 'iT wAs JuSt ThE WoRsT.');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  3,  5, 'it was the epoch of belief, it was the epoch of incredulity, ');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  1,  3, 'it was the season of Light, it was the season of Darkness, ');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  4,  4, 'it was the spring of hope, it was the winter of despair, ');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  1,  5, 'we had everything before us, we had nothing before us, ');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  2,  9, 'we were all going direct to Heaven, we were all going direct the other way—in short, ');
+-- INSERT INTO twists (story_id, author_id, parent_id, bodytext) VALUES (2,  3,  10, 'the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.');
