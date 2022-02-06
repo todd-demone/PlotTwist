a58d1ac89d5f66397aadb9ccd0330f2058d1272b
Todd Demone <todd.demone.dev@gmail.com>
2022-02-06 17:12:17 +0000

    replace getTwists queryString with a recursive/CTE query

--- db/twistsDb.js
+++ db/twistsDb.js
@@ -2,19 +2,51 @@ module.exports = (pool) => {
 
   // GET TWISTS FOR A STORY
   const getTwists = (story_id) => {
+    // const queryString = `
+    //   SELECT twists.*, users.username, newTwists.number_of_votes
+    //   FROM
+    //   (
+    //     SELECT twists.id as id, count(votes) as number_of_votes
+    //     FROM twists
+    //     JOIN votes ON twists.id = votes.twist_id
+    //     WHERE twists.story_id = $1
+    //     GROUP BY twists.id
+    //     ORDER BY parent_id NULLS FIRST
+    //   ) newTwists 
+    //   JOIN twists ON twists.id = newTwists.id
+    //   JOIN users ON twists.author_id = users.id;
+    // `;
     const queryString = `
-      SELECT twists.*, users.username, newTwists.number_of_votes
-      FROM
+      WITH RECURSIVE tp 
+      (id, story_id, author_id, parent_id, bodytext, accepted, date_created, username, number_of_votes, depth) AS 
       (
-        SELECT twists.id as id, count(votes) as number_of_votes
-        FROM twists
-        JOIN votes ON twists.id = votes.twist_id
-        WHERE twists.story_id = $1
-        GROUP BY twists.id
-        ORDER BY parent_id NULLS FIRST
-      ) newTwists 
-      JOIN twists ON twists.id = newTwists.id
-      JOIN users ON twists.author_id = users.id;
+          SELECT twists.id, twists.story_id, twists.author_id, twists.parent_id, twists.bodytext, twists.accepted, twists.date_created, users.username, twistsVotes.number_of_votes, 0 
+          FROM 
+          (
+            SELECT twists.id as id, count(votes) as number_of_votes
+            FROM twists
+            LEFT JOIN votes ON twists.id = votes.twist_id
+            WHERE twists.story_id = $1
+            GROUP BY twists.id
+          ) twistsVotes
+          JOIN twists ON twists.id = twistsVotes.id
+          JOIN users ON twists.author_id = users.id
+          WHERE twists.parent_id = 0
+        UNION ALL
+          SELECT twists.id, twists.story_id, twists.author_id, twists.parent_id, twists.bodytext, twists.accepted, twists.date_created, users.username, twistsVotes.number_of_votes, tp.depth + 1
+          FROM
+          (
+            SELECT twists.id as id, count(votes) as number_of_votes
+            FROM twists
+            LEFT JOIN votes ON twists.id = votes.twist_id
+            WHERE twists.story_id = $1
+            GROUP BY twists.id
+          ) twistsVotes
+          JOIN twists ON twists.id = twistsVotes.id
+          JOIN users ON twists.author_id = users.id
+          JOIN tp ON twists.parent_id = tp.id
+      )
+      SELECT * FROM tp ORDER BY depth, id;
     `;
     const queryParams = [story_id];
     return pool 
