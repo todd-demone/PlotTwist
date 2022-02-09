62f2337f65fbf67b7c900d5756386ce34776e352
Todd Demone <todd.demone.dev@gmail.com>
2022-02-09 16:38:18 +0000

    add usericon, username, date to accepted twist html template; add new twist form html template (display: none is default)

--- public/scripts/components/twist.js
+++ public/scripts/components/twist.js
@@ -3,7 +3,16 @@ $(() => {
   window.twist = {};
 
   function createAcceptedTwist(twist) {
-    return `<p class="accepted_twist__bodytext" style="background-color: white; margin: 0; padding-top: 1rem;">${twist.bodytext}</p>`;
+    return `
+      <div class="accepted_twist" style="background-color: white; margin-bottom: 2.5rem;">
+        <div class="accepted_twist__header" style="display: flex;">
+          <div class="accepted_twist__usericon"><img src="https://i.imgur.com/73hZDYK.png" width="25" height="25"></div>
+          <div class="accepted_twist__username">Posted by ${twist.username}</div>
+          <div class="accepted_twist__date">${moment(twist.date_created).fromNow()}</div>
+        </div>
+        <p class="accepted_twist__bodytext">${twist.bodytext}</p>
+      </div>
+    `;
   };
 
   function createUnacceptedTwist(twist, isOriginalAuthor, topLevel) {
@@ -18,9 +27,17 @@ $(() => {
       <div class="unaccepted_twist__footer" style="display: flex;">
         <div class="unaccepted_twist__vote_icon">⬆️</div>
         <div class="unaccepted_twist__vote_count">${twist.number_of_votes}</div>
-        <div class="new_twist_button">Twist It!</div>
+        <div class="unaccepted_twist__show_form_button">Twist It!</div>
         ${ isOriginalAuthor && topLevel === twist.depth ?  `<div class="unaccepted_twist__accept_button">Accept</div>` : `<div></div>`}
       </div>
+    </div>
+    <div class="new_twist" style="display: block; margin-left: calc(${twist.depth} * 20px);">
+      <form class="new_twist__form">
+        <input type="hidden" name="story_id" value="${twist.story_id}" readonly>
+        <input type="hidden" name="parent_id" value=${twist.parent_id}" readonly>
+        <textarea class="new_twist__textarea" name="bodytext" cols="30" rows="10" placeholder="Write a new twist..."></textarea>
+        <button class="new_twist__submit_button" type="submit">Submit</button>
+      </form>
     </div>
     `;
   };
