1283ec34c26c17c849212f6f7fb77c390cafcf53
Todd Demone <todd.demone.dev@gmail.com>
2022-02-09 16:36:06 +0000

    add code to stop render if story is complete; add call to createNewTwistForm(); begin drafting event handlers for this page

--- public/scripts/components/single_story.js
+++ public/scripts/components/single_story.js
@@ -38,6 +38,14 @@ $(() => {
       }
     }
     
+    // if story is marked 'completed', stop here - do not render newTwistForm or any unacceptedTwists
+    if (data.story.completed) {
+      return;
+    }
+
+    const newTwistFormEl = newTwistForm.createNewTwistForm(data.story);
+    addElement(newTwistFormEl);
+
     addElement(`<hr style="margin: 2.5rem 0;">`);
     
     function getNestedTwists(myParent_id) {
@@ -60,6 +68,50 @@ $(() => {
     }
   };
 
+  window.singleStory.clearSingleStory = clearSingleStory;
   window.singleStory.addSingleStory = addSingleStory;
+
+  ////////////////////
+  // Event Handlers //
+  ////////////////////
+  
+  // Mark a story complete 
+  $(".single_story").on("click", '.story__complete_button', function() {
+    window.singleStory.clearSingleStory();
+    // is the data object available here? can I add it to the event object in jquery?
+    completeStory(data.story.story_id) 
+      .then(function(response) {
+        window.singleStory.clearSingleStory();
+        window.singleStory.addSingleStory(response);
+        views_manager.show('singleStory');
+      });
+  });
+
+  // Submit a twist
+  const $newTwistForm = $('.new_twist__form')
+  
+  $newTwistForm.on("submit", function(event) {
+    const data = $newTwistForm.serialize();
+    event.preventDefault();
+    createTwist(data)
+      .then(function(response) {
+        // hide newTwistForm
+        // createUnacceptedTwist
+        // append unacceptedTwist below parent?
+      })
+    
+  });
+
+  // Vote for a twist
+  $(".single_story").on("click", '.unaccepted_twist__vote_icon', function() {
+  });
+
+  // Show the new twist form
+  $('.single_story').on('click', '.unaccepted_twist__show_form_button', function() {
+  });
+
+  // Accept a twist
+  $(".single_story").on('click', '.unaccepted_twist__accept_button', function() {
+  });
 
 });
