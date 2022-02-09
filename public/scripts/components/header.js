b47b5a2c01f5318722df14a3da5f73b7aa2e099c
Nico Dann <nicodann@gmail.com>
2022-02-09 19:57:39 +0000

    Add Home  redirect to Header Logo

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -1,12 +1,16 @@
 $(() => {
   const $pageHeader = $('#page-header')
   $pageHeader.append(`
-  <img src="public/media/workingLogo.png" alt="PlotTwist Logo">
+  <img src="media/workingLogo.png" alt="PlotTwist Logo" id="logo"></img>
   <!-- Button has been temporarily added for testing purposes -->
   <button class="single_story_button">Get SingleStory</button>
   <button id="create_story_button">Create Story</button>
   <div class="user-object">USEROBJECT</div>
   `)
+
+  $("body").on('click', "#logo", function() {
+    views_manager.show('stories');
+  })
 
   // This is here temporarily. It is used to test the singleStory.addSingleStory() function.
   $("body").on("click", ".single_story_button", function() {
