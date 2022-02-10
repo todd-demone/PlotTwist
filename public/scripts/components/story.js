59d94374c4b5286821e75527d7dc276af1c22472
Todd Demone <todd.demone.dev@gmail.com>
2022-02-10 19:01:34 +0000

    add 'mark story complete' handler to createStory() function

--- public/scripts/components/story.js
+++ public/scripts/components/story.js
@@ -3,8 +3,7 @@ $(() => {
   window.story = {};
 
   function createStory(story, isOriginalAuthor) {
-    console.log("inside createStory function")
-    return `
+    const $s = $(`
     <div class="story" style="background-color: white; margin-bottom: 2.5rem;">
       <div class="story__header" style="display: flex;">
         <div class="story__usericon"><img src="https://i.imgur.com/73hZDYK.png" width="25" height="25"></div>
@@ -15,7 +14,24 @@ $(() => {
       <h2 class="story__title">${story.title}</h2>
       <p class="story__bodytext" style="margin: 1rem 0 0;">${story.bodytext}</p>
     </div>
-    `;
+    `);
+
+    $s.on('click', '.story__complete_button', function() {
+      completeStory(story.id)
+        .then(function(result) {
+          return getStory(result.story.id);
+        })
+        .then(function(singleStoryData) {
+          window.singleStory.addSingleStory(singleStoryData);
+          views_manager.show('singleStory');
+        })
+        .catch(function(error) {
+          console.error(error);
+          views_manager.show('singleStory');
+        });
+    });
+
+    return $s;
   };
 
   window.story.createStory = createStory;
