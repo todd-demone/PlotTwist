404739ff19562420dcee9dbf4054be1622fa0008
Nico Dann <nicodann@gmail.com>
2022-02-10 14:38:15 +0000

    Add user object only when user logged inlog

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -6,27 +6,27 @@ $(() => {
         <button class="single_story_button">Get SingleStory</button>
         <button id="create_story_button">Create Story</button>
         `)
-  getUserObject()
-    .then(result => {
+        getUserObject()
+        .then(result => {
 
 
-      if (result) {
-        console.log("result!",result)
-        $pageHeader.append(`<div class="user-object">Greetings ${result.username}!</div>`)
-      }
+          if (result) {
+            console.log("result!",result)
+            $pageHeader.append(`<div class="user-object">Greetings ${result.username}!</div>`)
+          }
 
-      $("body").on('click', "#logo", function() {
-        views_manager.show('stories');
-      })
+          $("body").on('click', "#logo", function() {
+            views_manager.show('stories');
+          })
 
-      // This is here temporarily. It is used to test the singleStory.addSingleStory() function.
-      $("body").on("click", ".single_story_button", function() {
-        getStory(2)
-        .then(function(storyData) {
-          window.singleStory.addSingleStory(storyData);
-          views_manager.show('singleStory')
-        })
-      });
-    });
+        });
+        // This is here temporarily. It is used to test the singleStory.addSingleStory() function.
+        $("body").on("click", ".single_story_button", function() {
+          getStory(2)
+          .then(function(storyData) {
+            window.singleStory.addSingleStory(storyData);
+            views_manager.show('singleStory')
+          })
+        });
 
 });
