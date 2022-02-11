bd3daeab41a6646a8974178f719420bfd4811565
Todd Demone <todd.demone.dev@gmail.com>
2022-02-11 15:26:23 +0000

    merging of Nico and Todd's css

--- public/scripts/components/twist.js
+++ public/scripts/components/twist.js
@@ -4,9 +4,9 @@ $(() => {
 
   function createAcceptedTwist(twist) {
     return `
-      <div class="twist accepted_twist" style="background-color: white; margin-bottom: 2.5rem;">
-        <div class="twist__header accepted_twist__header" style="display: flex;">
-          <div class="twist__usericon accepted_twist__usericon"><img src="https://i.imgur.com/73hZDYK.png" width="25" height="25"></div>
+      <div class="twist accepted_twist">
+        <div class="twist__header accepted_twist__header">
+          <div class="twist__usericon accepted_twist__usericon"><i class="fa-regular fa-circle-user fa-xl"></i></div>
           <div class="twist__username accepted_twist__username">Posted by ${twist.username}</div>
           <div class="accepted_twist__date">${moment(twist.date_created).fromNow()}</div>
         </div>
@@ -19,14 +19,14 @@ $(() => {
     const $ut = $(`
     <div class="twist unaccepted_twist" style="margin-bottom: 2.5rem; margin-left: calc(${twist.depth} * 20px); background-color: white;">
       <div class="twist__header unaccepted_twist__header" style="display: flex;">
-        <div class="twist__usericon unaccepted_twist__usericon"><img src="https://i.imgur.com/73hZDYK.png" width="25" height="25"></div>
+        <div class="twist__usericon unaccepted_twist__usericon"><i class="fa-regular fa-circle-user fa-xl"></i></div>
         <div class="twist__username unaccepted_twist__username">Posted by ${twist.username}</div>
         <div class="unaccepted_twist__date">${moment(twist.date_created).fromNow()}</div>
       </div>
       <p class="unaccepted_twist__bodytext">${twist.bodytext}</p>
       <div class="unaccepted_twist__footer" style="display: flex;">
         <div class="votes">
-          <div class="unaccepted_twist__vote_icon">⬆️</div>
+          <div class="unaccepted_twist__vote_icon"><i class="fa-solid fa-circle-up fa-xl"></i></div>
           <div class="unaccepted_twist__vote_count">${twist.number_of_votes}</div>
         </div>
         <button class="unaccepted_twist__show_form_button">Twist It!</button>
@@ -42,7 +42,7 @@ $(() => {
     `);
 
     $ut.on('click', '.unaccepted_twist__show_form_button', function() {
-      $(this).parent().next().show();
+      $(this).parent().next().slideToggle();
     });
 
     $ut.on('submit', '.unaccepted_twist__form', function(e){
